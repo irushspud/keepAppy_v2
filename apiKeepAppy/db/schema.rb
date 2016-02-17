b63b0d7d1bfd18bc2a80a727c8bc6c815079d5ea
GordonHind96 <hindg@tcd.ie>
2016-02-17 21:06:49 +0000

    Added User(moderater) sign in scaffold with plans to add ability to remove posts and curate articles

--- apiKeepAppy/db/schema.rb
+++ apiKeepAppy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204112512) do
+ActiveRecord::Schema.define(version: 20160217201544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20160204112512) do
     t.text     "cite"
     t.string   "progenitor"
     t.integer  "classification"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "encrypted_password"
+    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
