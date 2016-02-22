3bfcc56d3c57f855ee08203d58f1b84106872e00
GordonHind96 <hindg@tcd.ie>
2016-02-22 13:50:49 +0000

    further changes

--- apiKeepAppy/app/controllers/sessions_controller.rb
+++ apiKeepAppy/app/controllers/sessions_controller.rb
@@ -2,18 +2,20 @@ class SessionsController < ApplicationController
   before_filter :authenticate_user, :only => [:home, :profile, :setting]
   before_filter :save_login_state, :only => [:login, :login_attempt]
   def login
+    #render "login"
   end
 
   def login_attempt
+    render "login"
     authorized_user = User.authenticate(params[:username_or_email],params[:login_password])
     if authorized_user
       session[:user_id] = authorized_user.id
-      flash[:notice] = "Wow Welcome again, you logged in as #{authorized_user.username}"
+      flash[:notice] = "Welcome again, you logged in as #{authorized_user.username}"
       redirect_to(:action => 'home')
     else
       flash[:notice] = "Invalid Username or Password"
       flash[:color]= "invalid"
-      render "login"	
+      #render(:action => 'login_at)	
     end
   end
 
@@ -23,8 +25,10 @@ class SessionsController < ApplicationController
   end
 
   def home
+    #render "home"
   end
 
   def setting
+    #render "setting"
   end
 end
--- apiKeepAppy/app/controllers/users_controller.rb
+++ apiKeepAppy/app/controllers/users_controller.rb
@@ -3,16 +3,22 @@ class UsersController < ApplicationController
   def new
 	@user = User.new
   end
+  
+  def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 
   def create
-	@user = User.new(params[:user])
+	@user = User.new(params[user_params])
 	if @user.save
 		flash[:notice] = "Sign up succesful"
 		flash[:color] = "valid"
+                redirect_to(:action => 'home')
 	else
 		flash[:notice] = "Form is invalid"
 		flash[:color] = "invalid"
 	end
-	render "new"
+	redirect_to(:controller => :sessions,:action => 'home')
+        
   end
 end
