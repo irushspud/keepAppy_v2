774f30c458cf8c699b3020ad1348036c799b1004
cifinn <ciaranfinn07@aol.com>
2016-03-17 14:36:20 +0000

    added production environment setup :: to deploy on heroku

--- apiKeepAppy/app/controllers/api/v1/articles_controller.rb
+++ apiKeepAppy/app/controllers/api/v1/articles_controller.rb
@@ -2,21 +2,22 @@ module Api
   module V1
     class ArticlesController < ApplicationController
       class Article < :: Article
-      def as_json(options={})
-        super.merge(image: image.url)
-      end
+      # def as_json(options={})
+      #   super.merge(image: image.url)
+      # end
       end
       respond_to :json
       #articles.json should be returned, but is returning old json object
       def index
-        respond_with Article.all
+        @articles = Article.all
+        render :json => @articles, :include => image_url(article.image)
       end
-     
+
       private
       def set_article
         @article = Article.find(params[:id])
       end
-      
+
       def article_params
         params.require(:article).permit(:title,:content,:author,:classification,:image)
       end
--- apiKeepAppy/app/views/articles/index.json.jbuilder
+++ apiKeepAppy/app/views/articles/index.json.jbuilder
@@ -2,6 +2,4 @@ json.array!(@articles) do |article|
   json.url article_url(article, format: :json)
   json.url image_url(article.image)
   json.extract!    article, :id, :title, :content, :author, :clssificaton, :image
-  
 end
-
