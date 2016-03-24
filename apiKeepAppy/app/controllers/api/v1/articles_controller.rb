13759cbda830bfe5094301a3806dd24eb920a520
GordonHind96 <hindg@tcd.ie>
2016-03-24 16:44:29 +0000

    Artiles api returns correct url for images should work if added on to end of base domain

--- apiKeepAppy/app/controllers/api/v1/articles_controller.rb
+++ apiKeepAppy/app/controllers/api/v1/articles_controller.rb
@@ -1,14 +1,14 @@
 module Api
   module V1
     class ArticlesController < ApplicationController
-
+	
       class Article < ::Article
         # Note: this does not take into consideration the create/update actions for changing released_on
         def as_json(options = {})
-          super.merge(tags: tag_list)
+          super.merge(tags: tag_list, image_url: image.url.sub("api/v1/articles_controller/",""))
         end
       end
-
+      before_filter :authenticate_user!
       respond_to :json
       #articles.json should be returned, but is returning old json object
       def index
