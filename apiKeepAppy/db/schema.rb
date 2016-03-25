ff202f2cf538a7cfc796d971da45567cd79242ad
GordonHind96 <hindg@tcd.ie>
2016-03-25 17:19:05 +0000

    added roles column to user table as opposed to relational table as users only have one role

--- apiKeepAppy/db/schema.rb
+++ apiKeepAppy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324131004) do
+ActiveRecord::Schema.define(version: 20160325171554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20160324131004) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "public_posts", force: true do |t|
+    t.text     "post"
+    t.integer  "rate"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "publicfeeds", force: true do |t|
@@ -92,6 +99,7 @@ ActiveRecord::Schema.define(version: 20160324131004) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
