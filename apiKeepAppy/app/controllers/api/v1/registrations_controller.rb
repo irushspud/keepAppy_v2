2b32af57115e379c26e4f76404b82ee6f92f5586
GordonHind96 <hindg@tcd.ie>
2016-03-31 17:32:58 +0100

    registration changes

--- apiKeepAppy/app/controllers/api/v1/registrations_controller.rb
+++ apiKeepAppy/app/controllers/api/v1/registrations_controller.rb
@@ -1,11 +1,11 @@
 module Api
   module V1
-    class RegistrationsController < Devise::RegistrationsController
+    class RegistrationsController < BaseController
   
       respond_to :json
       #handles registration of users from mobile
       def create
-        user = User.new(params[:user])
+        user = User.new(user_params)
         if user.save
           render :json=> user.as_json(:auth_token=>user.authentication_token, :email=>user.email), :status=>201
           return
@@ -13,6 +13,10 @@ module Api
           warden.custom_failure!
           render :json=> user.errors, :status=>422
         end
+
+        protected
+        def user_params
+          params.require(:user).permit(:email,:password,:password_confirmation)
       end
     end 
   end
